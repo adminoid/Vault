f824e84bcb0f0c7f238a8ea9a36595bcd0fea819
Petr Zenin <mr@adminoid.com>
2025-04-04 06:49:34 +0300

    some changes from MilkBowl to adminoid repo

--- .utility/deploy.sh
+++ .utility/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ "$TRAVIS_REPO_SLUG" != "MilkBowl/Vault" ]]
+if [[ "$TRAVIS_REPO_SLUG" != "adminoid/Vault" ]]
 then
         echo 'Travis can only publish docs for release builds.'
         return 0
